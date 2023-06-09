cb45572079d6f05e9e9c7fc4d17d1aed552daee1
aka. K00B404 aka. Bnonymous <goldenkooy@gmail.com>
2023-06-09 22:12:06 +0200

    Update color_reduced_symbols_grid_from_image.py

--- color_reduced_symbols_grid_from_image.py
+++ color_reduced_symbols_grid_from_image.py
@@ -12,37 +12,37 @@ import requests
 from ast import Param
 from PIL import Image, ImageColor, ImageEnhance
 
-
 os.system('mkdir -p /content/diamondcanvas')
 os.system('mkdir -p /content/diamondcanvas/images')
 os.system('mkdir -p /content/diamondcanvas/images/canvas')
 
-paper_formats= [['A0',[841,1180]],
-                ['A1',[594,840]],
-                ['A2',[420,594]],
-                ['A3',[297,420]]]
+paper_formats= [ ['A0',[841,1180]],['A1',[594,840]],['A2',[420,594]], ['A3',[297,420]],
+                 ['small_square',[50,50]], ['big_square',[1000,1000]] ]
+
+# !!!!!!! choose a paper format !!!!!!!!
+format = paper_formats[3] # A3
+# !!!!!!! number of colors in palette !!!!!!!!
+nr_of_colors = "64" 
+# !!!!!!! set this between 2.5 and 2.8 for round or squere drils !!!!!!!!
 cel_size = 2.5
+# to have a clean edge
 cells_away_from_edge = 2
-for i in range(len(paper_formats)):
+
+for i in range(len(paper_formats) -1):
+  # calculate and split image and grid sizes
   format_name =  paper_formats[i][0]
   paper_format = paper_formats[i][1]
   max_x_cells = paper_format[1] / cel_size
-  max_y_cells = paper_format[0] / cel_size
-  if len(paper_formats[i]) < 3:
-    paper_formats[i].append([str(round(max_y_cells - cells_away_from_edge)), str(round(max_x_cells - cells_away_from_edge))])
-    paper_formats[i].append(str(round((max_y_cells - cells_away_from_edge) * (max_x_cells - cells_away_from_edge))))
+  max_y_cells = paper_format[0] / cel_siz
+  #Store all values in arrays
+  paper_formats[i].append([str(round(max_y_cells - cells_away_from_edge)), str(round(max_x_cells - cells_away_from_edge))])
+  paper_formats[i].append(str(round((max_y_cells - cells_away_from_edge) * (max_x_cells - cells_away_from_edge))))
 
-
-format = paper_formats[3] # A3
 canvas_res = [format[1][1], format[1][0]] #resolution w/h 
 grid_res = [format[2][1], format[2][0]] #grid w/h 
 
 # 'https://assets.rebelmouse.io/media-library/junichi-shimodaira-s-rod-riguez.jpg?id=31159124&width={int(canvas_res[0])}&height={int(canvas_res[0])}&quality=100'
 input_img_url = 'https://as1.ftcdn.net/v2/jpg/05/57/06/50/1000_F_557065092_MLOigaEyhzfgKr5xxsn98IbRRaopL0Aw.jpg' #@param {type:"string"}
-
-nr_of_colors = "64" 
-print('max '++' colors')
-cel_size = 2.5 
 
 name, ext= os.path.splitext(input_img_url)
 
@@ -94,8 +94,8 @@ input_image = '/content/diamondcanvas/images/RGBA_art_url.png'
 # open the image
 im = Image.open(input_image)
 # using Image.ADAPTIVE to avoid dithering
-clrs = 48 # !!!!!!! number of colors in palette !!!!!!!!
-out = im.convert('P', palette=Image.ADAPTIVE, colors=clrs)
+
+out = im.convert('P', palette=Image.ADAPTIVE, colors=nr_of_colors)
 #get name and ext
 name, ext= os.path.splitext(input_image)
 # morph inputname into outputname
